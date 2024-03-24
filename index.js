92ac6cc1461a811f526f1ac9205fca130db8238d
dfghjkcvbnmnbvcx <162885162+qwertyu34567@users.noreply.github.com>
2024-03-24 13:53:34 +0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+// Hàm đơn giản trả về "Hello, world!"
+function sayHello() {
+    return "Hello, world!";
+}
+
+// Xuất hàm để có thể sử dụng nó từ các module khác
+module.exports = { sayHello };
